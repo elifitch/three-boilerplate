9190fbe445961f390ff5947d10d53d6f8524dfd4
Eli Fitch <eli.fitch.prof@gmail.com>
2017-12-08 21:54:45 -0500

    including resize logic

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,13 @@ import ExampleCube from './example-cube';
 import 'three/OrbitControls';
 
 const containerEl = document.getElementsByClassName('container')[0];
+const cW = containerEl.offsetWidth;
+const cH = containerEl.offsetHeight;
 
 const renderer = Renderer({containerEl, clearColor: 0xEFEFEF});
 const { scene, camera } = Scene({
   cameraPos: [10, 0, 0],
-  cameraAspect: containerEl.offsetWidth / containerEl.offsetHeight,
+  cameraAspect: cW / cH,
   cameraFov: 45
 });
 
@@ -23,5 +25,11 @@ controls.rotateSpeed = 0.5;
 controls.dampingFactor = 0.25;
 
 scene.add(ExampleCube({fragmentShader: frag}));
+
+window.addEventListener('resize', () => {
+  renderer.setSize(cW, cH);
+  camera.aspect = cW / cH;
+  camera.updateProjectionMatrix();
+});
 
 RenderLoop({renderer, scene, camera, controls});
